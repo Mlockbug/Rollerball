3d162dcc4c244152acef1e5ccd9fde60c2184bf0
Mlockbug <energetichamster@gmail.com>
2022-09-08 15:21:05 +0100

    v1.0.0
    First completed build

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
@@ -8,6 +10,9 @@ public class playerController : MonoBehaviour
     public float moveSpeed = 0f;
     private Rigidbody rb;
     Vector2 movementVector;
+    private int score = 0;
+    public TextMeshProUGUI scoreCount;
+    public GameObject winText;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,10 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (score == 12)
+        {
+            winText.SetActive(true);
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -29,5 +37,15 @@ public class playerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.AddForce(new Vector3(movementVector.x,0f,movementVector.y) * moveSpeed);
+    }
+
+    void OnTriggerEnter(Collider pickup)
+    {
+        if (pickup.tag == "pickup")
+        {
+            pickup.gameObject.SetActive(false);
+            score++;
+            scoreCount.text = "Score: " + score.ToString();
+        }
     }
 }
